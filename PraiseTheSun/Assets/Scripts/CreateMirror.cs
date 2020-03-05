aea62d10a8144b833a6aac06f471d48420477567
Adam Olsson <adaols80@gmail.com>
2020-03-05 15:26:11 +0100

    Start to implement the game

---
+++ PraiseTheSun/Assets/Scripts/CreateMirror.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMirror : MonoBehaviour {
+
+    public Vector3 mousepos;
+    Vector3 sun_position;
+
+    // Start is called before the first frame update
+    void Start() {
+        sun_position = GameObject.FindGameObjectWithTag("Sun").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (Input.GetMouseButtonDown(0)) {
+
+            mousepos = new Vector3(Input.mousePosition.x, sun_position.y, Input.mousePosition.z);
+            Vector3 newPos = Camera.main.WorldToViewportPoint(mousepos);
+
+            newPos.y = sun_position.y;
+
+            GameObject startPoint = new GameObject();
+            startPoint.transform.position = newPos;
+
+        }
+    }
+}
---
+++ PraiseTheSun/Assets/Scripts/OrbitObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitObject : MonoBehaviour {
+
+    public float OrbitDegrees;
+    public GameObject orbit_object;
+
+    private Vector3 orbitPosition;
+
+
+    void Start() {
+        orbitPosition = orbit_object.transform.position;
+    }
+
+
+    void Update() {
+        transform.position = RotatePointAroundPivot(transform.position, orbitPosition, Quaternion.Euler(0, OrbitDegrees * Time.deltaTime, 0));
+    }
+
+    public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle) {
+        return angle * (point - pivot) + pivot;
+    }
+}

f6b715015ba7a4c8ca9318fa90ab134dc28ee1d5
Adam Olsson <adaols80@gmail.com>
2020-03-10 11:10:20 +0100

    Auto stash before merge of "master" and "origin/master"

--- PraiseTheSun/Assets/Scripts/CreateMirror.cs
+++ PraiseTheSun/Assets/Scripts/CreateMirror.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 public class CreateMirror : MonoBehaviour {
 
-    public Vector3 mousepos;
-    Vector3 sun_position;
+    private Vector3 sun_position;
+    private GameObject mouse_position;
+    public float speed = 20.0f;
+
+    public List<Transform> mirror_transform;
 
     // Start is called before the first frame update
     void Start() {
         sun_position = GameObject.FindGameObjectWithTag("Sun").transform.position;
+        mouse_position = GameObject.FindGameObjectWithTag("mouse");
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        mouse_position.transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update() {
-        
+        float x = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        float y = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+        mouse_position.transform.Translate(x, y, 0);
         if (Input.GetMouseButtonDown(0)) {
-
-            mousepos = new Vector3(Input.mousePosition.x, sun_position.y, Input.mousePosition.z);
-            Vector3 newPos = Camera.main.WorldToViewportPoint(mousepos);
-
+            Vector3 newPos = mouse_position.transform.position;
             newPos.y = sun_position.y;
 
             GameObject startPoint = new GameObject();
+            startPoint.name = "startMirror" + (mirror_transform.Count / 2);
+            startPoint.tag = "mirror";
             startPoint.transform.position = newPos;
-
+            mirror_transform.Add(startPoint.transform);
         }
+        if (Input.GetMouseButtonUp(0)) {
+            Vector3 newPos = mouse_position.transform.position;
+            newPos.y = sun_position.y;
+
+            GameObject endPoint = new GameObject();
+            endPoint.name = "endMirror" + (mirror_transform.Count / 2);
+            endPoint.tag = "mirror";
+            endPoint.transform.position = newPos;
+
+            mirror_transform.Add(endPoint.transform);
+
+            createLineRenderer(mirror_transform[mirror_transform.Count - 2].transform, endPoint.transform);
+        }
+    }
+
+    void createLineRenderer(Transform startPos, Transform endPos) {
+        LineRenderer lineRenderer = endPos.gameObject.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, endPos.position);
+        lineRenderer.SetPosition(1, startPos.position);
+        lineRenderer.widthCurve = AnimationCurve.Linear(0, 0.1f, 1, 0.1f);
     }
 }
